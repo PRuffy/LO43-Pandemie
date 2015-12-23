014edaaa7e8f3cb0c5ddd36f5bcbde8188cf674d
Notmoo <skraig.boeuf@gmail.com>
2015-12-23 23:49:27 +0100

    Correctif erreurs de compilations
    Correction des erreurs empêchant la compilation du programme.
    Multiples modifications de commentaire dans les classes concernées.

--- src/Jeu.java
+++ src/Jeu.java
@@ -14,21 +14,21 @@ public class Jeu {
     private boolean projetLEIM;
     private boolean projetRT;
     private int chargeTravail;
-    private int compteurSurchargeTravail;
+    private int compteurEclosion;
 
     private static final int MAX_ECLOSION = 8;
     private static final int [] CHARGE_TRAVAIL = new int[]{2,2,3,3,4};
 
 
     /*constructeur de jeu. Prends les parametre suivant :
-     * int nombre de joueur : Permet de créer le nombre de joueur désiré. Les différents rôle seront choisis aléatoirement
-     * string listUV : Donner après la lecture du document UV.txt, permet de générer les UV
-     * int[][] adjacence : permet d'obtenir la liste des voisin
+     * int nombre de joueur : Permet d'avoir le nombre de joueur désiré. Les différents rôles seront choisis aléatoirement
+     * string listUV : Donnée après la lecture du document UV.txt, permet de générer les UV
+     * int[][] adjacence : permet d'obtenir la liste des voisins
      * 
      */
     public Jeu(int nombreDeJoueurs){
         chargeTravail = CHARGE_TRAVAIL[0];
-        compteurSurchargeTravail = 0;
+        compteurEclosion = 0;
 
         projetI2RV = false;
         projetILC = false;
@@ -44,7 +44,7 @@ public class Jeu {
 
 
 
-    /*Augmente le nombre de marqueur sur une UV donner*/
+    /*Augmente le nombre de marqueur sur une UV donnée*/
     public void augmenterChargeTravail(CarteInfection carte){
         Filiere carteFilière = carte.getFiliere();
         UV cible = carte.getCible();
@@ -60,16 +60,16 @@ public class Jeu {
 
     /*Lors d'une éclosion la charge de travail augmente*/
     public void augmenterEclosion(){
-        if(compteurSurchargeTravail != MAX_ECLOSION) {
-            compteurSurchargeTravail++;
-            chargeTravail = CHARGE_TRAVAIL[compteurSurchargeTravail];
+        if(compteurEclosion != MAX_ECLOSION) {
+            compteurEclosion++;
+            chargeTravail = CHARGE_TRAVAIL[compteurEclosion];
         }
     }
 
 
 
-    /*Enleve les marqueurs sur une uv donner.
-     *Si le joueur et le surdoué alors enlève tout les marqueurs*/
+    /*Enleve les marqueurs sur une uv donnée.
+     *Si le joueur est le surdoué alors enlève tous les marqueurs*/
     public void travailler(){
         int positionPersonnage = joueurActif.getPosition();
         UV uvTravail = graph.getUV(positionPersonnage);
@@ -87,10 +87,10 @@ public class Jeu {
     }
 
 
-    /*Fonction executant la pioche des cartes semestre en fin de tour de joueur.
-     *Si la carte et une carte TP elle est envoyé en mains du joueur
+    /*Fonction exécutant la pioche des cartes semestre en fin de tour de joueur.
+     *Si la carte est une carte TP elle est envoyé en main du joueur
      *Sauf si le joueur a atteint la limite de carte
-     *Sinon si carte Bénéfique stocker dans l'arrayliste des carte bénéfique jusqu'à utilisation
+     *Sinon si carte Bénéfique stockée dans l'Arraylist des cartes bénéfiques jusqu'à utilisation
      *Sinon déclenche une éclosion = Appel de la méthode éclosion
      */
     public void piocheSemestre(){
@@ -101,7 +101,16 @@ public class Jeu {
                 if(joueurActif.getMainComplete()){
                     carteSemestre.defausserCarte(tempCarte);
                 }else{
-                    joueurActif.ajoutCarte(tempCarte);
+                    try{
+                        joueurActif.ajoutCarte(tempCarte);
+                    }
+                    /*
+                    On catch les exceptions correspondant au manque de place dans la main du joueur et
+                    aux erreurs de type de cartes possibles venant de la pioche
+                    */
+                    catch(NotEnoughSlotsException e){System.out.println("Le joueur n'a pas assez de place dans sa main");}
+                    catch(WrongTypeException e){System.out.println("Les joueurs ne peuvent avoir que des cartes TP en main");}
+
                 }
             }else if(tempCarte.getType() == TypeCarteSemestre.CC){
                 carteSemestre.defausserCarte(tempCarte);
@@ -137,8 +146,9 @@ public class Jeu {
 
     }
 
-    /*Fonction déplacement un joueur. Reçois un joueur a déplacer et la position ou il doit arriver
-     *N'effectue pas de test vis a vis de la position atteignable.
+    /*
+     * Fonction déplacement un joueur. Reçoit un joueur à déplacer et la position où il doit arriver
+     * N'effectue pas de test vis a vis de la position atteignable.
      * Peut déplacer le joueur Actif comme un autre joueur
      */
     public void deplacer(Joueur joueurCible,int positionArriver){
@@ -146,7 +156,7 @@ public class Jeu {
         reduireAction();
     }
 
-    /*Fonction ammenant le prof de la filière sur l'UV ou se trouve l'élève*/
+    /*Fonction ammenant le prof de la filière sur l'UV où se trouve l'élève*/
     public void appelerProf(){
         int temPositionJoueur = joueurActif.getPosition();
         Filiere f = graph.getUVFiliere(temPositionJoueur);
@@ -154,19 +164,19 @@ public class Jeu {
     }
 
     /*Fonction permettant de rendre un projet. Dépense des cartes.
-     *Ne fonctionne que si un professeur se trouve sur la même case que le joueurAcif*/
+     *Ne fonctionne que si un professeur se trouve sur la même case que le joueurActif*/
     public void rendreProjet(){
 
         reduireAction();
     }
 
-    /*Set le nombre d'action restante du joueur actif a 0 pour ensuite lancer la fin du tour*/
+    /*Set le nombre d'actions restantes du joueur actif a 0 pour ensuite lancer la fin du tour*/
     public void passer(){
         joueurActif.setNombreAction(0);
         finDeTour();
     }
 
-    /*Renvoi le boolean associé au rendu de projet pour la filiere envoyé*/
+    /*Renvoit le boolean associé au rendu de projet pour la filiere envoyé*/
     private boolean testRenduProjet(Filiere f){
         if (f == Filiere.RT){
             return projetRT;
@@ -174,7 +184,7 @@ public class Jeu {
             return projetI2RV;
         }else if (f == Filiere.LEIM){
             return projetLEIM;
-        }else if(f == Filiere.ILC){
+        }else{
             return projetILC;
         }
 
