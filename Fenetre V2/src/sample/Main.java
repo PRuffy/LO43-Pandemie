090a7d8f0831f9aa480e9a6989f35c40e90707fb
ThibaultTsagalos <thibault.tsagalos@utbm.fr>
2016-01-03 07:50:11 +0800

    Oups
    hem petite bétise de ma part ca ouvrait pas la bonne scene x)

--- Fenetre V2/src/sample/Main.java
+++ Fenetre V2/src/sample/Main.java
@@ -794,7 +794,7 @@ public class Main extends Application {
                 primaryStage.setScene(scene1);
             }
         });
-        primaryStage.setScene(scene2);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
